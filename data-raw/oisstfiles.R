eccaad0308b5e1c44364ff9d8ec9fc529bca7cdd
Michael Sumner <mdsumner@gmail.com>
2024-02-06 00:28:07 +0000

    vrt protocol

--- data-raw/oisstfiles.R
+++ data-raw/oisstfiles.R
@@ -4,7 +4,7 @@ oisst <- raadfiles::oisst_daily_files()
 
 ## vrt suffix
 vrtix <- "?a_srs=EPSG:4326&projwin=0,60,360,-60&sd_name=sst"
-oisst$dsn <- paste0(gsub("/rdsi/PUBLIC/raad/data/", "/vsicurl/https:///", oisst$fullname), vrtix, sep = "")
+oisst$dsn <- paste0(gsub("/rdsi/PUBLIC/raad/data/", "vrt:///vsicurl/https:///", oisst$fullname), vrtix, sep = "")
 oisst$fullname <- NULL
 oisst$root <- NULL
 
